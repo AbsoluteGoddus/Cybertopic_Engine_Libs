238c687d57a08e16311ad6cffa996a58e907c8b6
AbsoluteGoddus <138155542+AbsoluteGoddus@users.noreply.github.com>
2023-09-15 14:13:36 +0200

    Update cymath_angles.h

--- cymath/cymath_angles.h
+++ cymath/cymath_angles.h
@@ -1,4 +1,23 @@
 #ifndef CYBERTOPIC_ENGINE_1_CYMATH_ANGLES_H
 #def CYBERTOPIC_ENGINE_1_CYMATH_ANGLES_H
 
+#include <cymath_std.h>
+
+namespace cy
+{
+  namespace angle
+  {
+    class quaternion
+    {
+      
+    }
+
+    class euler
+    {
+      public:
+        
+    }
+  }
+}
+
 #endif //CYBERTOPIC_ENGINE_1_CYMATH_ANGLE_H
