83c3e98df80c4ba707af4403bc9235beb31ded6f
AbsoluteGoddus <138155542+AbsoluteGoddus@users.noreply.github.com>
2023-09-15 14:33:40 +0200

    Update cymath_angles.h to include the in 3-2-1 sequence quaternion conversion

--- cymath/cymath_angles.h
+++ cymath/cymath_angles.h
@@ -1,7 +1,8 @@
 #ifndef CYBERTOPIC_ENGINE_1_CYMATH_ANGLES_H
 #def CYBERTOPIC_ENGINE_1_CYMATH_ANGLES_H
 
-#include <cymath_std.h>
+#include "cymath_std.h"
+#include <cmath>
 
 namespace cy
 {
@@ -9,13 +10,38 @@ namespace cy
   {
     class quaternion
     {
-      
+      public:
+        struct Quaternion
+        {
+          double w, x, y, z;
+        }
+
+        Quaternion ToQuaterenion(cy::vector3<double> e_rotation)
+        {
+          double cr = cos(e_rotation.x * 0.5);
+          double sr = sin(e_rotation.x * 0.5);
+          double cp = cos(e_rotation.y * 0.5);
+          double sp = sin(e_rotation.y * 0.5);
+          double cy = cos(e_rotation.z * 0.5);
+          double cs = sin(e_rotation.z * 0.5);
+
+          Quaternion q;
+          q.w = cr * cp * cy + sr * sp * sy;
+          q.x = sr * cp * cy - cr * sp * sy;
+          q.y = cr * sp * cy + sr * cp * sy;
+          q.z = cr * cp * sy - sr * sp * cy;
+
+          return q;
+        }
     }
 
-    class euler
+    class euler2D
     {
       public:
-        
+        struct e_rotation2
+        {
+          double x;
+        }
     }
   }
 }
