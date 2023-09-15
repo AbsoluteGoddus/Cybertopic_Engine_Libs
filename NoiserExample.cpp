0b1471c758b0d6adccc9defa6057a2845729389d
AbsoluteGoddus <138155542+AbsoluteGoddus@users.noreply.github.com>
2023-09-15 13:59:34 +0200

    Create NoiserExample.cpp

---
+++ NoiserExample.cpp
@@ -0,0 +1,11 @@
+#include <CE_main.h>
+
+int main()
+{
+  ns::perlinNoise prln(12345);
+  cy::vector2<float> myvector2;
+  myvector2.x = 1.013;
+  myvector2.y = myvector.x;
+  prln.create(myvector.x, myvector.y)
+  return 0;
+}
